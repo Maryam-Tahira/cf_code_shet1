f8b4c016779dd070f9975fdebfbb525834ea308d
Maryam Tahira <tahiramaryam34@gmail.com>
2025-04-03 22:26:24 +0600

    Add files via upload

---
+++ codeforces area.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    float R;
+    float pi=3.141592653;
+    float Area;
+    cin>>R;
+    Area=pi*R*R;
+    printf("%.9f",Area);
+    return 0;
+}
+
